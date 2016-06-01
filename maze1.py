53bb9cab82af54323b2ba0fe9c96b7695b50d9a2
Jeremy A Kansas <ymerejsasnak@gmail.com>
2016-06-01 14:07:54 -0400

    working on fitness, population, reproduction, etc...getting good

--- maze1.py
+++ maze1.py
@@ -9,28 +9,29 @@ CELL_SIZE = 40
 WALL = '#'
 START = 'S'
 FINISH = 'F'
-ZONES = [str(x) for x in range(1, 10)]  #strings '1' thru '9' part of determining fitness
+ZONES = [str(x) for x in range(10)]  #strings '0' thru '9' part of determining fitness
 
 WALL_COLOR = (20, 20 ,20)
 FLOOR_COLOR = (230, 230, 230)
 START_COLOR = (100, 200, 100)
 FINISH_COLOR = (200, 100, 100)
 
-GENOME_LENGTH = 200
+GENOME_LENGTH = 300
 NORTH = (0, -1)
 EAST = (1, 0)
 SOUTH = (0, 1)
 WEST = (-1, 0)
 GENES = (NORTH, EAST, SOUTH, WEST)
 
-GENERATION_SIZE = 20
-REPRODUCER_GROUP_SIZE = 5
+GENERATION_SIZE = 1000
+REPRODUCER_GROUP_SIZE = 100
+MUTATION_RATE = 20
 
 
 
 '''notes:
-guys will be circles (that fit w/in cells...radius 1/2 cell size or maybe a bit less)
-genome will have how many moves?  1000?
+PROBLEMS!
+to fix: gen size to 1, uncomment delay, print out fitness to watch as it goes...etc
 fitness will start at a set value (1000?) then subtract one for each movement (so getting
 to finish faster is better), subtract more for hitting a wall, then give multiplier(?)
 bonus for landing in better 'zones' (to reward getting closer to finish) 
@@ -44,24 +45,24 @@ bonus for landing in better 'zones' (to reward getting closer to finish)
 
 # maze representation ideas --- replace . with numbers representing zones to grade fitness
 MAZE1 = ['####################',
-         '#22222222222###FFFF#',
-         '#22222222222###5555#',
-         '#22222222222###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###3333###5555#',
-         '#1111###44444444444#',
-         '#1111###44444444444#',
-         '#S111###44444444444#',
+         '#33333333333###FFFF#',
+         '#33333333333###9999#',
+         '#33333333444###9999#',
+         '#3333###4444###9999#',
+         '#2222###4444###9999#',
+         '#2222###4444###9999#',
+         '#2222###4444###8888#',
+         '#2222###4444###8888#',
+         '#1111###4444###8888#',
+         '#1111###4444###8888#',
+         '#1111###4444###8888#',
+         '#1111###5555###7777#',
+         '#1111###5555###7777#',
+         '#1111###5555###7777#',
+         '#0000###5555###7777#',
+         '#0000###55556667777#',
+         '#0000###55666666666#',
+         '#S000###55666666666#',
          '####################']
          
 
@@ -107,23 +108,25 @@ class Runner:
         
         self.finished = False
 
-        self.fitness = 1000 # for now, will have to tweak this...
+        self.fitness = 0
 
     def move(self, cells, turn):
+        if self.finished:
+            return
+            
         direction = self.genome[turn]
         target = (self.position[0] + direction[0], self.position[1] + direction[1])
-        if cells[target] == WALL:
-            self.fitness -= 10
-        else:
+        if cells[target] != WALL:
             self.position = target
-            self.fitness -= 1
+            if cells[self.position] in ZONES:
+                self.fitness += int(cells[self.position]) * 2
             
-        if cells[target] == FINISH:
-            self.finished == True
-            self.fitness *= 10
+        if cells[self.position] == FINISH:
+            self.finished = True
+            self.fitness *= 500 - turn  # major fitness bonus for finishing
             
-        if turn == GENOME_LENGTH - 1 and self.position in ZONES:
-            self.fitness *= int(cells[self.position])
+        if turn == GENOME_LENGTH - 1 and cells[self.position] in ZONES:
+            self.fitness *= int(cells[self.position]) * 2
     
     def draw(self, screen):
         size = CELL_SIZE
@@ -144,16 +147,19 @@ class GA:
     
     def next_generation(self, start_position):
         # sort based on fitness
-        self.generation.sort(key=lambda runner: runner.fitness)
+        self.generation.sort(key=lambda runner: runner.fitness, reverse=True)
         
         next_gen = []
         for i in range(GENERATION_SIZE):
             parents = r.sample(self.generation[:REPRODUCER_GROUP_SIZE], 2)
-            genome_split = r.randint(GENOME_LENGTH // 3, GENOME_LENGTH // 3 * 2)
+            genome_split = r.randint(GENOME_LENGTH // 5, GENOME_LENGTH // 5 * 4)
             child_genome = parents[0].genome[:genome_split] + parents[1].genome[genome_split:]
             
-            
-            # mutate
+            #mutate
+            for gene in child_genome:
+                if MUTATION_RATE < r.randrange(100):
+                    gene = r.choice(GENES)
+                    
             
             
             next_gen.append(Runner(start_position, child_genome))
@@ -195,7 +201,9 @@ def run():
             pygame.display.flip()
             
             #pygame.time.delay(100)
+        print(str(len(ga.generation)))
         
+        print('gen: ', str(ga.generation_count), '\t\t', 'best fit: ', int(ga.generation[0].fitness))
         ga.next_generation(grid.start_position)
 
 run()
